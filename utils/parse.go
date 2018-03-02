c71759cc63185c1efa9d12a5321be3b55ed1f829
Zack Patrick <zpatrick319@Hotmail.com>
2018-03-01 17:30:27 -0800

    add slack user parser

--- utils/parse.go
+++ utils/parse.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 	"unicode"
 )
@@ -35,4 +37,14 @@ func ParseShell(input string) []string {
 	}
 
 	return args
+}
+
+func ParseSlackUser(escaped string) (string, error) {
+	// escaped user format: '<@ABC123>'
+	r := regexp.MustCompile("\\<\\@[a-zA-Z0-9]+\\>")
+	if !r.MatchString(escaped) {
+		return "", fmt.Errorf("Invalid user: please enter a valid user by typing '@<username>'")
+	}
+
+	return escaped[2 : len(escaped)-1], nil
 }
