7b83209fee5c88be75c995c258887ffd6e59e810
Zack Patrick <zpatrick319@Hotmail.com>
2018-03-15 12:05:04 -0700

    remove unused utils. Add runner for interview cleanups

---
+++ runner/runner_test.go
@@ -0,0 +1,41 @@
+package runner
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunnerRun(t *testing.T) {
+	var called bool
+	r := NewRunner("", func() error {
+		called = true
+		return nil
+	})
+
+	if err := r.Run(); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.True(t, called)
+}
+
+func TestRunnerRunEvery(t *testing.T) {
+	c := make(chan bool)
+	r := NewRunner("", func() error {
+		c <- true
+		return nil
+	})
+
+	ticker := r.RunEvery(time.Nanosecond)
+	defer ticker.Stop()
+
+	for i := 0; i < 5; i++ {
+		select {
+		case <-c:
+		case <-time.After(time.Millisecond):
+			t.Fatal("timeout")
+		}
+	}
+}
